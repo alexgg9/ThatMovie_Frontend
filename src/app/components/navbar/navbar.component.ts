1adc15d68dac4cdad900107e8025a2e664f22f61
rafa30052003 <rafalucenavalle9@gmail.com>
2024-02-13 13:57:04 +0100

    relogic search

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { Movie } from 'src/app/model/movie';
-import { MovieResponse } from 'src/app/model/movieResponse';
-import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,55 +8,17 @@ import { MovieService } from 'src/app/services/movie.service';
   standalone: true,
   imports: [IonicModule],
 })
-export class NavbarComponent  implements OnInit {
+export class NavbarComponent  {
   @Input() title: string = 'ThatMovie';
-  searchQuery: string | undefined;
-  movies: Movie[] | undefined;
-  filteredMovies: Movie[] = [];
+  @Output() onSearch: EventEmitter<string> = new EventEmitter();
 
 
-
-  constructor(private movieService: MovieService) {
-    this.searchMovies('');
-  }
-
-  ngOnInit() {
-   
-  }
 
   onSearchInput(event: CustomEvent) {
-    this.searchQuery = (event.target as HTMLInputElement).value;
-    if (this.movies && this.searchQuery) {
-      this.filteredMovies = this.movies.filter((movie) =>
-      this.searchQuery &&  movie.title?.toLowerCase().includes(this.searchQuery?.toLowerCase())
-      );
-    }
-    this.searchMovies(this.searchQuery); 
+    const searchQuery = (event.target as HTMLInputElement).value;
+    this.onSearch.emit(searchQuery);
   }
 
-   searchMovies(query: string) {
-    this.movieService.getSearchMovies(query).subscribe(
-      (response: any) => {
-        if (response.results) {
-          this.movies = response.results.map((movie: any) => ({
-            title: movie.title,
-            // Otras propiedades que necesites mapear
-          }));
-        } else {
-          console.error('No se encontraron películas en la respuesta.');
-          this.movies = [];
-        }
-      },
-      (error: any) => {
-        console.error('Error al obtener películas:', error);
-        this.movies = [];
-      }
-    );
-  }
 
-  onMovieSelected(event: any) {
-    // Manejar la selección de la película aquí
-    console.log('Película seleccionada:', event);
-  }
 
 }
