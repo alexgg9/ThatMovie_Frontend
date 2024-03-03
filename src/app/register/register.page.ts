e46d438120f2d073b04ff501ccbeebe993911af4
rafa30052003 <rafalucenavalle9@gmail.com>
2024-03-03 18:35:20 +0100

    login and register

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private LoginService: LoginService) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      name: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      surname: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      // Llamar al servicio de registro con los datos del formulario
+      this.LoginService.getRegister(this.registerForm.value).subscribe(
+        (response) => {
+          // Manejar la respuesta del servidor, por ejemplo, mostrar un mensaje de éxito
+        },
+        (error) => {
+          // Manejar el error, por ejemplo, mostrar un mensaje de error al usuario
+        }
+      );
+    } else {
+      // Manejar el caso en el que el formulario no es válido, por ejemplo, mostrar mensajes de validación
+    }
   }
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { environment } from 'src/environments/environment';
 import { Member } from '../model/member';
+import { LoginRequest } from '../login/LoginRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,8 @@ export class LoginService {
   constructor(private http: HttpClient) {}
 
 
-  getlogin(email: string, password: string): Observable<any> {
-    return this.http.post<any>(this.apiLogin, { email, password });
+  getlogin(loginRequest: LoginRequest): Observable<LoginRequest> {
+    return this.http.post<LoginRequest>(this.apiLogin, { ...loginRequest });
   }
   getRegister(member : Member): Observable<Member> {
     return this.http.post<Member>(this.apiRegister, member);
